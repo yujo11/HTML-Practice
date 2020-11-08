2f95bc0588af6dea4e75fe6f06078548d47d7f72
yujo <yujo@student.42seoul.kr>
2020-11-08 14:28:20 +0900

    Circle Drawing

--- canvas/circle_drawing/app.js
+++ canvas/circle_drawing/app.js
@@ -1,5 +1,38 @@
 class App {
-  //
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    window.addEventListener('resize', this.resize.bind(this), false);
+    this.resize();
+
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+  }
+
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+    this.ctx.fillStyle = '#cddb49';
+    this.ctx.beginPath();
+
+    this.ctx.arc(this.stageWidth / 2, this.stageHeight / 2, 300, 0, 2 * Math.PI);
+
+    this.ctx.fill();
+  }
 }
 
 window.onload = () => {
